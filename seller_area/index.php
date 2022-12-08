66bc071182bc3ed28fba0b9fe2914d92f697b077
Jaycie <jaycie.delacruz12@gmail.com>
2022-12-08 14:26:26 +0800

    Add seller dashboard

---
+++ seller_area/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seller Dashboard</title>
+    <!-- Bootstrap CSS Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Font Awesome Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+       <!-- CSS file -->
+       <link rel="stylesheet" href="../styles.css">
+</head>
+
+<body>
+        <!-- navbar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #563D7C;">
+            <div class="container-fluid">
+                <img src="../images/logo.png" class="logo" alt="logo">
+                <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #563D7C;">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link text-white fw-bold"><i class="fa-solid fa-user fa-xl"></i> Welcome guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </div>
+        <!-- second child -->
+        <div class="secondary text-white">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+    <!-- Bootstrap JS Link -->
+</body>
+</html>
