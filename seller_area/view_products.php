32c696130a91673b1b289144387dec8aef9737fa
cookiedino19 <119904138+cookiedino19@users.noreply.github.com>
2022-12-15 17:30:46 +0800

    Update view_products.php

--- seller_area/view_products.php
+++ seller_area/view_products.php
@@ -1,2 +1,84 @@
 <h1 class='text-center'>Products</h1>
-                
+<div class="row">
+        <div class="col-md-10">
+            <div class="row">
+<div class="col-md-4 mb-2">
+<div class="card" style="width: 90%">
+                    <img src="../images/bear-brand-milk-320.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-secondary">View More</a>
+                      </div>
+                    </div>
+                </div>
+                      <div class="col-md-4  mb-2">
+                <div class="card" style="width: 90%;">
+                    <img src="../images/jack-daniels.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-secondary">View More</a>
+                    </div>
+                    </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/tender-juicy.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/tender-juicy.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/tender-juicy.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/pancit-canton.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/magnum.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/pineapple-juice.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
