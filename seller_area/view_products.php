11b8e57afa328d2f5a7d45cdeb37eefbb1efecc1
cookiedino19 <119904138+cookiedino19@users.noreply.github.com>
2022-12-15 20:17:22 +0800

    Update view_products.php

--- seller_area/view_products.php
+++ seller_area/view_products.php
@@ -82,6 +82,16 @@
                 </div>
                 </div>
                 </div>
+                <div class="col-md-4 mb-2">
+                <div class="card" style="width: 90%;">
+                <img src="../images/pineapple-juice.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-secondary">View More</a>
+                </div>
+                </div>
+                </div>
 </div>
 </div>
 </div>
