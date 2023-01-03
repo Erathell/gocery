a85dd33c128dfdc166e6db22949a055650defb91
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-03 11:40:05 +0800

    Add duplicate checking functionality for add_categories.php

--- seller_area/add_category.php
+++ seller_area/add_category.php
@@ -2,10 +2,19 @@
     include('../includes/connect.php');
     if(isset($_POST['insert_category'])){
         $category_title=$_POST['category_title'];
-        $insert_query="insert into `categories` (category_title) values ('$category_title')";
-        $result=mysqli_query($con, $insert_query);
-        if($result){
-            echo "<script>alert('Category has been added successfully')</script>";
+        // select data from database
+        $select_query="Select * from `categories` where category_title='$category_title'";
+        $result_select=mysqli_query($con, $select_query);
+        $number=mysqli_num_rows($result_select);
+        if($number>0){
+            echo "<script>alert('Category already exists')</script>";
+        }else{
+
+            $insert_query="insert into `categories` (category_title) values ('$category_title')";
+            $result=mysqli_query($con, $insert_query);
+            if($result){
+                echo "<script>alert('Category has been added successfully')</script>";
+            }
         }
     }
 ?>
