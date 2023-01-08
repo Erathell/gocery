663ab80c8cebb1dd9996e5ba06717f316264db03
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-08 14:22:01 +0800

    add getProducts function

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     include('includes/connect.php');
+    include('functions/common_function.php');
 ?>
 
 <!DOCTYPE html>
@@ -74,26 +75,8 @@
             <div class="row">
                 <!--fetching products-->
                 <?php
-                $select_query = "Select * from `products` order by rand()";
-                $result_query = mysqli_query($con, $select_query);
-                while($row = mysqli_fetch_assoc($result_query)){
-                    $product_id =$row['product_id'];
-                    $product_title = $row['name'];
-                    $product_description = $row['product_description'];
-                    $product_category = $row['category_id'];
-                    $product_image = $row['product_image'];
-                    $product_price = $row['product_price'];
-                    echo "<div class='col-md-4 mb-2'>
-                    <div class='card' style='width: 90%'>
-                        <img src='product_images/$product_image' class='card-img-top' alt='$product_title'>
-                        <div class='card-body'>
-                            <h5 class='card-title'>$product_title</h5>
-                            <p class='card-text'>$product_description</p>
-                            <a href='#' style='background-color:#42C84F; border-color: #42C84F;' class='btn btn-primary'>Add to Cart</a>
-                            <a href='#' class='btn btn-secondary'>View More</a>
-                        </div>
-                    </div>
-                </div>";}
+                    getProducts()
+                
                 ?>
             <!--row end -->
             </div>
