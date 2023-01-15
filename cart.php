17d18171b05fa323917c64032e4169b99a2532ed
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-15 11:30:42 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -101,28 +101,32 @@
                                                 $product_title=$row_product_price['name'];
                                                 $product_image=$row_product_price['product_image'];
                                                 $product_values=array_sum($product_price);
-                                                $total_price+=$product_values;                            
+                                                                            
                                     ?>
                                     <tr>
                                         <td><?php echo $product_title?></td>
                                         <td><img src="./product_images/<?php echo $product_image ?>" class="cart_img" alt="sus"></td>
-                                        <td><input type="text" name="qty" id="" class="form-input w-50" ></td> <?php
+                                        <td>
+                                            <input type="hidden" name="cart_id" class="form-input w-50" value = <?php echo $row['quantity'] ?>>
+                                            <input type="number" name="qty" class="form-input w-50" value = <?php echo $row['quantity'] ?>></td> <?php
                                             $get_ip = getIPAddress();
-                                            if(isset($_POST['update_cart'])){
-                                                $quantities=$_POST['qty'];
-                                                $update_cart="update `cart` set quantity=$quantities where ip_address='$get_ip' and product_id=2";
-                                                $result=mysqli_query($con, $update_cart);
-                                                $total_price=$total_price*$quantities;
-                                            }
+                                            // if(isset($_POST['update_cart'])){
+                                            //     $quantities=$_POST['qty'];
+                                            //     $update_cart="update `cart` set quantity=$quantities where ip_address='$get_ip' and product_id=2";
+                                            //     $result=mysqli_query($con, $update_cart);
+                                            //     $total_price=$total_price*$quantities;
+                                            // }
                                          ?>
-                                        <td>₱ <?php echo $price_table?></td>
+                                        <td>₱ <?php echo $sub_total = number_format($row_product_price['product_price'] * $row['quantity'])?></td>
                                         <td><input type="checkbox" name="removeitem[]" value="<?php echo $product_id?>"></td>
                                         <td>
                                             <button type="submit" value="updatecart" name="update_cart" class="btn btn-warning btn-rounded me-3">Update</button>
                                             <button type="submit" value="Remove Cart"name="remove_cart" class="btn btn-danger btn-rounded">Delete</button>        
                                         </td>
                                     </tr>
-                                <?php }
+                                <?php
+                                    $total_price+=$sub_total;
+                                    }
                                 } ?>
                                 </tbody>
                             </table>
