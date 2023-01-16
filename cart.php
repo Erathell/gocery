c20212ff8672568452a8914fd8823053bcf5326a
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-01-17 03:04:14 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -97,7 +97,7 @@
                 </li>";
                 }else{
                     echo "<li class='nav-item'>
-                    <a class='nav-link' style='color: white;' href=''>Logout</a>
+                    <a class='nav-link' style='color: white;' href='#'>Logout</a>
                 </li>";
                 }
                 ?>
