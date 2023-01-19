fac815148bbdcfec98ba03ac0d3140302746b44f
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-19 11:12:35 +0800

    Update order.php

--- user_area/order.php
+++ user_area/order.php
@@ -8,7 +8,15 @@
 
         // getting total items and total price
         $get_ip_address = getIPAddress();
-        
+        $total_price = 0;
+        $cart_query_price = "Select * from `cart` where ip_address='$get_ip_address'";
+        $result_cart_price=mysqli_query($con, $cart_query_price);
+        $count_products=mysqli_num_rows($result_cart_price);
+        while($row_price=mysqli_fetch_array($result_cart_price)){
+            $product_id=$row_price['product_id'];
+
+        }
+
 
     }
 ?>
