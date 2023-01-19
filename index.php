8d4accc26bc63b8ba7726851a224449f569f17be
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-19 13:06:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                         ?></sup> Cart</strong></a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link nav-button" href="#"><i class="fa-solid fa-hand-holding-dollar fa-xl "></i><strong> Total Price: ₱ <?php total_cart_price() ?></strong> </a>
+                    <span class="nav-link nav-button"><i class=" fa-solid fa-hand-holding-dollar fa-xl "></i><strong> Total Price: ₱ <?php total_cart_price() ?></strong></span>
                     </li>
                 </ul>
                 <form class="d-flex" action="search_product.php" method="get">
