239a99b731af7269526161b88d1624b3c5759b58
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-24 23:20:35 +0800

    upload_image

--- seller_area/upload_image.php
+++ seller_area/upload_image.php
@@ -3,12 +3,14 @@
 
     if(isset($_POST['submit'])){
         $seller_img=$_FILES['file']['name'];
-        $seller_img_temp=$_FILES['file']['temp_name'];
+        $seller_img_temp=$_FILES['file']['tmp_name'];
         move_uploaded_file($seller_img_temp, "../images/$seller_img");
 
         $insert_query="update `seller` set `seller_img`='$seller_img'";
         $run=mysqli_query($con, $insert_query);
+        if($run){
         echo "<script>alert('Upload success')</script>";
+        }
     }
 ?>
 <form action="" method="post" enctype="multipart/form-data">
