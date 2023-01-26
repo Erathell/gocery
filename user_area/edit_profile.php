e29c83631cc8cc0185cf0fe8d8ae238ca85a4195
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-01-26 23:42:56 +0800

    Update edit_profile.php

--- user_area/edit_profile.php
+++ user_area/edit_profile.php
@@ -125,7 +125,7 @@ if (isset($_POST['save_image'])) {
     $rows_count = mysqli_num_rows($result);
     $name_data = mysqli_fetch_assoc($result);
     
-    if($rows_count != 0){
+    if($rows_count > 1){
       $ne_error='Email aldready taken';
     }
     else{
