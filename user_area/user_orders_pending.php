e830307190ec9456fe5ff9dd09cbb242a39a4c08
Jaycie <jaycie.delacruz12@gmail.com>
2023-01-27 10:32:44 +0800

    Update user_orders_pending.php

--- user_area/user_orders_pending.php
+++ user_area/user_orders_pending.php
@@ -49,10 +49,14 @@
             $date = $row_orders['date'];
             $order_status = $row_orders['order_status'];
             $product_id = $row_orders['product_id'];
+            $get_prod_details = "Select * from `products` where product_id= '$product_id'";
+            $result_products = mysqli_query($con, $get_prod_details);
+            $row_prod = mysqli_fetch_assoc($result_products); 
+            $product_name=$row_prod['name'];
             echo "<tr class='table-hover-orange'>
             <th scope='row' >$order_id</th>
             <td class='mb-2'>Php $amount</td>
-            <td>$quantity</td>
+            <td>$product_name</td>
             <td>$date</td>
             <td>$order_status</td>
             <td><button type='submit' name='confirm' class='btn btn-green-purple rounded-pill'>Confirm</button></td>
