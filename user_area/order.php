24e2da8b0485d4bd68c29313841786092167e43d
Overseer24 <66957205+Overseer24@users.noreply.github.com>
2023-01-28 17:59:09 +0800

    pending

--- user_area/order.php
+++ user_area/order.php
@@ -12,7 +12,7 @@
         $cart_query_price = "Select * from `cart` where ip_address='$get_ip_address'";
         $result_cart_price=mysqli_query($con, $cart_query_price);
         $count_products=mysqli_num_rows($result_cart_price);
-        $status='pending';
+        $status='PENDING';
         while($row_price=mysqli_fetch_array($result_cart_price)){
             $product_id=$row_price['product_id'];
             $select_product="Select * from `products` where product_id=$product_id";
